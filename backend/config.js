c2d178065c4ab50505abbbfe9c563b866d5cb4ef
Mukul Jadon <147290552+MukulJadon20@users.noreply.github.com>
2024-10-17 12:04:44 +0530

    commit

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,5 @@
 export const PORT = 8000;
 
-export const mongoDBURL ="mongodb://localhost:27017";
-
-
+// export const mongoDBURL ="mongodb://localhost:27017";
+export const mongoDBURL='mongodb+srv://mukuljadon:Mukul%402001@cluster0.wm50s.mongodb.net/book';
 //  module.exports=PORT;
